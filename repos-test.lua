6f816eac378379e10419d1f6b4b7a6730a024b4a
David Leal <halfpacho@gmail.com>
2023-06-21 21:19:17 +0000

    Initial project setup

---
+++ repos-test.lua
@@ -0,0 +1,23 @@
+-- USED FOR TESTING PURPOSES ONLY.
+
+local repos = {
+    {
+        name = "Minetest-WorldEdit",
+        url = "https://github.com/Uberi/Minetest-WorldEdit",
+        dir = "libs/"
+    },
+    {
+        name = "panqkart",
+        url = "https://github.com/panqkart/panqkart",
+        dir = "libs/"
+    },
+    {
+        name = "cloud_items",
+        url = "https://github.com/minetest-mods/cloud_items",
+        dir = "libs/"
+    }
+}
+
+return {
+    repos = repos
+}
