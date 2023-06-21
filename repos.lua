5bff80ed3e9e1789f81b6c91b5971ef500d12d6e
David Leal <halfpacho@gmail.com>
2023-06-21 21:19:17 +0000

    Initial project setup (WIP)

---
+++ repos.lua
@@ -0,0 +1,16 @@
+local repos = {
+    {
+        name = "Minetest-WorldEdit",
+        url = "https://github.com/Uberi/Minetest-WorldEdit",
+        dir = "libs/"
+    },
+    {
+        name = "panqkart",
+        url = "https://github.com/panqkart/panqkart",
+        dir = "libs/"
+    }
+}
+
+return {
+    repos = repos
+}
