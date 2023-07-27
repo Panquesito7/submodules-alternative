794636971e8c2569859eb4dbc953a90ebe8ad237
David Leal <halfpacho@gmail.com>
2023-07-27 10:14:54 -0600

    Minor tweaks in `update-repos.lua`

--- update-repos.lua
+++ update-repos.lua
@@ -47,6 +47,14 @@ else
     one_pr = "false"
 end
 
+-- Commit message option.
+local commit_message
+if arg[4] ~= nil then
+    commit_message = arg[4]
+else
+    commit_message = "Bump repositories to their latest version"
+end
+
 --- @brief Updates all the repositories by
 --- running `git pull` on each repository.
 --- Nothing will be done if the repositories are not behind the remote.
@@ -102,7 +110,7 @@ local function update_repos()
             os.execute("git config --local user.name github-actions[bot]")
             os.execute("git config --local user.email 'github-actions@users.noreply.github.com'")
 
-            os.execute("git commit -m \"Bump " .. repos[i].name .. " to its latest commit\"")
+            os.execute("git commit -m \"Bump `" .. repos[i].name .. "` to its latest commit\"")
         end
 
         if one_pr == "false" and squash_commits == "false" then
@@ -113,7 +121,7 @@ local function update_repos()
     end
 
     if squash_commits == "true" and one_pr == "true" then
-        os.execute("git commit -m \"" .. arg[4] .. "\"")
+        os.execute("git commit -m \"" .. commit_message .. "\"")
     end
 end
 
