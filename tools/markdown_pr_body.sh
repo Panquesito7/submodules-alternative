08d8dc40d0d73286519c1af45d12d08168e49b24
David Leal <halfpacho@gmail.com>
2023-09-20 15:47:30 +0000

    Various fixes

--- tools/markdown_pr_body.sh
+++ tools/markdown_pr_body.sh
@@ -1,18 +1,24 @@
 # Arguments:
 # [0]: Taken by Bash, the name of the script
 # [1]: The mode that will be used to generate the PR body. Available modes are: "all", "updated", "added"
-# [2]:
-# Is it separated or not?
+# [2]: Is it separated or not?
 # If it's separated, each subtree PR will use `pr_body_update_separate.txt` and put their subtree name replacing
 # `{updated_subtree}`. Available options are: `true` or `false`
 #
-# [3]: The subtree name that will be used to replace `{updated_subtree}` in `pr_body_update_separate.txt`.
+# [3]: The action path to obtain the necessary files.
+# [4]: The subtree name that will be used to replace `{updated_subtree}` in `pr_body_update_separate.txt`.
 
-if [ "$1" == "all" ] || [ "$1" == "" ];
+if ([ "$1" == "all" ] || [ "$1" == "" ]) && [ "$2" == "false" ];
 then
     # Reads the files and stores the lines in arrays
-    mapfile -t updated_subtrees < updated_subtrees.txt
-    mapfile -t added_subtrees < added_subtrees.txt
+
+    if [ -f updated_subtrees.txt ]; then
+        mapfile -t updated_subtrees < updated_subtrees.txt
+    fi
+
+    if [ -f added_subtrees.txt ]; then
+        mapfile -t added_subtrees < added_subtrees.txt
+    fi
 
     # Adds backticks around each subtree name
     updated_subtrees=("${updated_subtrees[@]/#/\\\`}")
@@ -23,7 +29,7 @@ then
     added_subtrees_md=$(printf -- '- %s\\n' "${added_subtrees[@]}")
 
     # Creates a temporary file for the PR body
-    cat pr_body.txt > TEMP.txt
+    cat "$3/tools/pr_body.txt" > TEMP.txt
 
     if test -s updated_subtrees.txt;
     then
@@ -47,17 +53,13 @@ then
         echo "Various subtrees were updated or added in this PR." > TEMP.txt
     fi
 
-    # Prints the PR body
-    cat TEMP.txt
-
-    # Delete the temporary PR body file
-    rm TEMP.txt
-
-elif [ "$1" == "updated" ];
+elif [ "$1" == "updated" ] && [ "$2" == "false" ];
 then
 
     # Reads the files and stores the lines in arrays
-    mapfile -t updated_subtrees < updated_subtrees.txt
+    if [ -f updated_subtrees.txt ]; then
+        mapfile -t updated_subtrees < updated_subtrees.txt
+    fi
 
     # Adds backticks around each subtree name
     updated_subtrees=("${updated_subtrees[@]/#/\\\`}")
@@ -66,19 +68,15 @@ then
     updated_subtrees_md=$(printf -- '- %s\\n' "${updated_subtrees[@]}")
 
     # Creates a temporary file for the PR body
-    cat pr_body_update.txt > TEMP.txt
+    cat "$3/tools/pr_body_update.txt" > TEMP.txt
 
-    # Prints the PR body
-    cat TEMP.txt
-
-    # Delete the temporary PR body file
-    rm TEMP.txt
-
-elif [ "$1" == "added" ];
+elif [ "$1" == "added" ] && [ "$2" == "false" ];
 then
 
     # Reads the files and stores the lines in arrays
-    mapfile -t added_subtrees < added_subtrees.txt
+    if [ -f added_subtrees.txt ]; then
+        mapfile -t added_subtrees < added_subtrees.txt
+    fi
 
     # Adds backticks around each subtree name
     added_subtrees=("${added_subtrees[@]/#/\\\`}")
@@ -87,33 +85,24 @@ then
     added_subtrees_md=$(printf -- '- %s\\n' "${added_subtrees[@]}")
 
     # Creates a temporary file for the PR body
-    cat pr_body_fetch.txt > TEMP.txt
+    cat "$3/tools/pr_body_fetch.txt" > TEMP.txt
 
-    # Prints the PR body
-    cat TEMP.txt
-
-    # Delete the temporary PR body file
-    rm TEMP.txt
-
-    else
-        echo "Invalid mode. Available modes are: \"all\", \"updated\", \"added\""
-        exit 1
+elif [ "$1" != "all" ] && [ "$1" != "updated" ] && [ "$1" != "added" ]; then
+    echo "Invalid mode. Available modes are: \"all\", \"updated\", \"added\""
+    exit 1
 fi
 
-if [ "$2" == "true" ];
+if [ "$2" == "true" ] && [ "$1" == "updated" ];
 then
         # Creates a temporary file for the PR body
-        cat pr_body_update_separate.txt > TEMP.txt
+        cat "$3/tools/pr_body_update_separate.txt" > TEMP.txt
 
         # Add backticks around the subtree name
-        updated_subtree="\\\`$3\\\`"
+        updated_subtree="\\\`$4\\\`"
 
         # Replace the updated subtree name
         sed -i -e "s|{updated_subtree}|$updated_subtree|g" TEMP.txt
-
-        # Prints the PR body
-        cat TEMP.txt
-
-        # Delete the temporary PR body file
-        rm TEMP.txt
 fi
+
+# Prints the PR body
+cat TEMP.txt
